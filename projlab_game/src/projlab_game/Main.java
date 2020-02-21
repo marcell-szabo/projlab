2402bf99b68bd39a38e7f496ea4718b8f39cef4d
kingajohanna <szabo.kinga1999@gmail.com>
2020-02-21 21:45:18 +0100

    ASD

--- projlab_game/src/projlab_game/Main.java
+++ projlab_game/src/projlab_game/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//KUGLÓFAAAASZ
-		//sikerülni fog a push ;)
+		//sikerülni fog a push<3
 
 
 	}
