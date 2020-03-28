e392f17b7327527066637c040b6405e1f6ab9774
guszty <auguszta123@gmail.com>
2020-03-28 22:07:12 +0000

    Siker

--- sarkvidek/src/skeleton/Main.java
+++ sarkvidek/src/skeleton/Main.java
@@ -3,7 +3,7 @@ package skeleton;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hola");
+        System.out.println("Hello");
         Skeleton skel = new Skeleton();
         skel.runSkeleton();
     }
