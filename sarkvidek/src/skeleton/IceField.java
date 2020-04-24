1e1b5000ad9dfcd8046a68bdc071098abb8fe9df
guszty <auguszta123@gmail.com>
2020-04-24 22:19:56 +0200

    no message

--- sarkvidek/src/skeleton/IceField.java
+++ sarkvidek/src/skeleton/IceField.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 import skeleton.Result;
 
-import static skeleton.Result.OK;
+import static skeleton.Result.*;
 
 /**
  * Jégtáblák/Jégmezõk kezelésére szolgáló osztály. Egyrészt a jégmezõk teherbírásának vizsgálatát végzi el, másrészt
@@ -12,16 +12,22 @@ import static skeleton.Result.OK;
  * (van-e igloo vagy nincs) milyen következményekkel jár (mezõn levõ hórétegek számát mindig növeljük, viszont a
  * testhõ csökkentése csak az iglooval védetlen mezõkön történik meg).
  */
-public class IceField extends Field {
+public class IceField extends Field
+{
     /**
-     * Megadja, hogy adott mezõ tartalmaz-e igloot. (Ha tartalmaz, akkor TRUE az értéke)
+     * Megadja, hogy adott jégmezõre (icefield-re) van-e építve bármi.
      */
-    private boolean igloo;
+    protected Igloo protection;
+    /**
+     * Tárolja, hogy a mezõn milyen tárgyat lehet felvenni.
+     */
+    protected Item item;
 
     /**
      * Default constructor
      */
-    public IceField() {
+    public IceField()
+    {
         super();
     }
 
@@ -32,36 +38,39 @@ public class IceField extends Field {
      *
      * @return
      */
-    public Result storm() {
-        Result r = Result.OK;
-        System.out.print(this.toString() + ".storm();\n");
-        System.out.print("Van iglu a jegtablan? i/n \n");
-        Scanner scan = new Scanner(System.in);
-        char c = scan.next().charAt(0);
-        if (c == 'n') {
-            //TODO
-            Eskimo esk = new Eskimo(new Game(), this);
-            Explorer exp = new Explorer(new Game(), this);
-            players.add(esk);
-            players.add(exp);
-            for (Player p : players) {
-                r = p.decreaseHeat();
+    public Result storm()
+    {
+        snow++;
+        Result r = NOTHING;
+        if (protection == null)
+        {
+            for (Player ps : players)
+            {
+                r = ps.decreaseHeat();
+                if (r == DIE)
+                    return r;
             }
         }
-        System.out.print(this.toString() + ".storm() returned Result r;\n");
         return r;
     }
 
     /**
-     * A Field osztályban lévõ absztrakt függvény megvalósítása. OK értékkel tér vissza, ha az adott mezõn lévõ
-     * játékosok számát még elbírja a jégtábla. Ellenkezõ esetben pedig DIE értékkel fog visszatérni.
+     * A Field osztályban található absztrakt függvény megvalósítása.
+     * Legelõször megnézi, hogy tartózkodik-e az adott mezõn jegesmedve, ha igen, megnézi, hogy a rajta levõ igloo megvédi-e tõle,
+     * ha nem, akkor a függvény DIE-al tér vissza.
+     * Ha ezt a vizsgálatot követõen nem keletkezett visszatérési érték, akkor az adott
+     * mezõn lévõ játékosok száma kerül vizsgálatra. Amennyiben elbírja az ott tartózkodó karaktereket a
+     * jégtábla, akkor OK, ellenkezõ esetben DIE értékkel tér vissza a metódus.
      *
      * @param p
      * @return Result
      */
-    public Result stepOn(Player p) {
-        System.out.println(this.toString() + ".stepOn(p1);");
-        System.out.println(this.toString() + ".stepOn(p1) returned Result OK;");
+    public Result stepOn(Player p)
+    {
+        if (polarbear.actualfield == this && !protection.protectFromBear())
+            return  DIE;
+        if (players.size() > capacity)
+            return  DIE;
         return OK;
     }
 
@@ -70,22 +79,21 @@ public class IceField extends Field {
      *
      * @return
      */
-    public boolean haveIgloo() {
-        // TODO implement here
-        return false;
+    public boolean haveIgloo()
+    {
+        return protection != null;
     }
 
     /**
-     * A Field osztály virtuális buildIgloo() metódusának a felüldefiniálása. Akkor hívódik meg, ha egy eszkimó
-     * igloot szeretne építeni a jégtáblán. Ha még nem volt igloo a mezõn, akkor az igloo attribútum értékét
-     * TRUE-ra állítja, majd OK értékkel tér vissza. Amennyiben volt igloo az adott, mezõn, akkor NOTHING lesz
-     * a visszatérési értéke.
+     * A Field osztály virtuális build(Igloo) metódusának a felüldefiniálása. Megvizsgálja, hogy a mezõn, található-e építmény(sátor vagy igloo).
+     * Ha igen, akkor visszatér NOTHING-gal, különben pedig OK-kal
      *
      * @return res Result
      */
-    public Result buildIgloo() {
-        System.out.println(this.toString() + ".buildIgloo();");
-        System.out.println(this.toString() + ".buildIgloo() returned Result OK;");
+    public Result build(Igloo i)
+    {
+        if (protection == null)
+            return NOTHING;
         return OK;
     }
 
@@ -94,130 +102,37 @@ public class IceField extends Field {
      * akkor meghívja az Item osztály pickMeUp(Player) függvényét.
      * Sikeres tárgyfelvétel esetén OK-kal tér vissza, egyébként pedig NOTHING-gal.
      *
-     * @return REsult az eredménnyel
+     * @return Result az eredménnyel
      */
-    public Result pickUp(Player p) {
-
-        List<Tool> tools;
-        Result res;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Adja meg a kivant kodot\n" + "1. Aso felvetele\n2. Kotel felvetele\n"
-                + "3. Buvarruha felvetele\n4. Etel elfogyasztasa\n5. Jelzoraketa alkatreszenek felvetele");
-        int tool = scan.nextInt();
-        System.out.println(tool);
-        switch (tool) {
-            case 1:
-                System.out.println("Aso felvetele\n");
-                System.out.println("1. Van mar asoja\n2. Nincs asoja");
-                int shovel = scan.nextInt();
-                System.out.println(shovel);
-                switch (shovel) {
-                    case 1:
-                        System.out.println("Van mar asoja");
-                        System.out.println(this.toString() + ".pickUp(e);");
-                        tools = p.getTools();
-                        System.out.println("ice.pickUp(e) returned Result res;\n\n");
-                        break;
-                    case 2:
-                        System.out.println("Nincs asoja");
-                        System.out.println(this.toString() + ".pickUp(e);");
-                        tools = p.getTools();
-                        item = new Shovel();
-                        res = item.pickMeUp(p);
-                        System.out.println("ice.pickUp(e) returned Result res;\n\n");
-                        break;
-                    default:
-                        System.out.println("Helytelen ertek\n");
-                        break;
-
-                }
-                break;
-            case 2:
-                System.out.println("Kotel felvetele\n");
-                System.out.println("1. Van mar kotele\n2. Nincs kotele");
-                int rope = scan.nextInt();
-                System.out.println(rope);
-                switch (rope) {
-                    case 1:
-                        System.out.println("Van mar kotele");
-                        System.out.println(this.toString() + ".pickUp(e);");
-                        tools = p.getTools();
-                        System.out.println("ice.pickUp(e) returned Result res;\n\n");
-                        break;
-                    case 2:
-                        System.out.println("Nincs kotele");
-                        System.out.println(this.toString() + ".pickUp(e);");
-                        tools = p.getTools();
-                        item = new Rope();
-                        res = item.pickMeUp(p);
-                        System.out.println("ice.pickUp(e) returned Result res;\n\n");
-                        break;
-                    default:
-                        System.out.println("Helytelen ertek\n");
-                        break;
-                }
-                break;
-            case 3:
-                System.out.println("Buvarruha felvetele\n");
-                System.out.println("1. Van mar buvarruhaja\n2. Nincs buvarruhaja");
-                int divingsuit = scan.nextInt();
-                System.out.println(divingsuit);
-                switch (divingsuit) {
-                    case 1:
-                        System.out.println("Van mar buvarruhaja");
-                        System.out.println(this.toString() + ".pickUp(e);");
-                        tools = p.getTools();
-                        System.out.println("ice.pickUp(e) returned Result res;\n\n");
-                        break;
-                    case 2:
-                        System.out.println("Nincs buvarruhaja");
-                        System.out.println(this.toString() + ".pickUp(e);");
-                        tools = p.getTools();
-                        item = new DivingSuit();
-                        res = item.pickMeUp(p);
-                        System.out.println("ice.pickUp(e) returned Result res;\n\n");
-                        break;
-                    default:
-                        System.out.println("Helytelen ertek\n");
-                        break;
-                }
-                break;
-            case 4:
-                System.out.println("Etel fogyasztasa");
-                System.out.println("1. A testoje maximumon van\n2. Nincs maximumon a testhoje");
-                int food = scan.nextInt();
-                System.out.println(food);
-                switch (food) {
-                    case 1:
-                        System.out.println("A testoje maximumon van");
-                        System.out.println(this.toString() + ".pickUp(e);");
-                        tools = p.getTools();
-                        System.out.println("ice.pickUp(e) returned Result res;\n\n");
-                        break;
-                    case 2:
-                        System.out.println("Nincs maximumon a testhoje");
-                        System.out.println(this.toString() + ".pickUp(e);");
-                        tools = p.getTools();
-                        item = new Food();
-                        res = item.pickMeUp(p);
-                        System.out.println("ice.pickUp(e) returned Result res;\n\n");
-                        break;
-                    default:
-                        System.out.println("Helytelen ertek\n");
-                        break;
-                }
-                break;
-            case 5:
-                System.out.println("Jelzoraketa alkatreszenek felvetele");
-                System.out.println(this.toString() + ".pickUp(e);");
-                tools = p.getTools();
-                item = new FlareGun();
-                res = item.pickMeUp(p);
-                System.out.println("ice.pickUp(e) returned Result res;\n\n");
-                break;
+    public Result pickUp(Player p)
+    {
+        if(snow == 0 && item != null)
+        {
+            return item.pickMeUp(p);
         }
-        return null;
+        return NOTHING;
     }
 
+    @Override
+    public Result stepOn(Polarbear pb)
+    {
+        polarbear = pb;
+        if(players == null)
+            return OK;
+        else if (protection == null)
+            return  DIE;
+        else if (protection.ProtectFromBear())
+            return  OK;
+        return DIE;
+    }
+
+    public void aging()
+    {
+        if (protection != null)
+        {
+            if (protection.aging() == DISAPPEAR)
+                protection = null;
+        }
+    }
 
 }
